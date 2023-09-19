dd590260012dfd1a06653dec855665a1e2fcd05b
Akshai-Kanna <akshaikannao1@gmail.com>
2023-09-19 18:20:04 +0530

    Building the first Sample API

--- src/main/java/com/akshai/springdemo/student/StudentController.java
+++ src/main/java/com/akshai/springdemo/student/StudentController.java
@@ -2,9 +2,7 @@ package com.akshai.springdemo.student;
 
 import com.fasterxml.jackson.databind.annotation.JsonAppend;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -20,10 +18,32 @@ public class StudentController {
         this.service = service;
     }
 
+    @PostMapping
+    public Student save(@RequestBody Student student){
+        return service.save(student);
+    }
+
+    @GetMapping("/{email}")
+    public Student findByEmail(@PathVariable("email") String email){
+        return service.findByEmail(email);
+    }
+
+
     @GetMapping
     public List<Student> findAllStudents(){
 
         return service.findAllStudents();
     }
+
+    @PutMapping
+    public Student updateStudent(@RequestBody Student student){
+        return service.update(student);
+    }
+
+    @DeleteMapping("/{email}")
+    public void delete(@PathVariable("email") String email){
+        service.delete(email);
+    }
+
 
 }
--- src/main/java/com/akshai/springdemo/student/StudentService.java
+++ src/main/java/com/akshai/springdemo/student/StudentService.java
@@ -5,29 +5,13 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.List;
 
-@Service
-public class StudentService {
+public interface StudentService {
 
-    public List<Student> findAllStudents(){
-
-        return List.of(
-                new Student(
-                        "Hello",
-                        "World",
-                        LocalDate.now(),
-                        "akshaikanna01@gmail.com",
-                        21
-                ),
-
-                new Student(
-                        "Hellllll",
-                        "World",
-                        LocalDate.now(),
-                        "akshaikanna01@gmail.com",
-                        22
-                )
-        );
-    }
+    Student save(Student s);
+    List<Student> findAllStudents();
+    Student findByEmail(String email);
+    Student update(Student s);
+    void delete(String email);
 
 
 }
